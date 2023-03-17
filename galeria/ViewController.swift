62479c142da75d2a1e2a104c835a7028364d2f67
AlekseyOberstein <420035969@pcpuma.acatlan.unam.mx>
2023-03-17 11:29:37 -0600

    Initial Commit

---
+++ galeria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  galeria
+//
+//  Created by CEDAM17 on 17/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
